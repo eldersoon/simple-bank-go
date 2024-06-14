d22cc9972a8f49fbf741d9c9a14c72427887d0dc
Elderson Gama <elderson.gama@globalsys.com.br>
2024-06-14 00:15:46 -0300

    fix: adjust err condition

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -30,7 +30,7 @@ func (store *Store) execTx (ctx context.Context, fn func(*Queries) error) error 
 	err = fn(q)
 
 	if err != nil {
-		if rbErr := tx.Rollback(); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
 	}
